c9e315b341e881b9ba62f457f408986ddcc3bf30
Aarmastah <aarpears@hotmail.com>
2021-10-19 17:28:50 -0400

    firewall

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install lib32z1 libncurses5:i386 libbz2-1.0:i386 lib32gcc-s1 lib32stdc++6 libtinfo5:i386 libcurl3-gnutls:i386 -y
+sudo ufw allow 22
+sudo ufw allow 27015
+sudo ufw enable
 wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 tar zxf steamcmd_linux.tar.gz
 rm -rf steamcmd_linux.tar.gz
