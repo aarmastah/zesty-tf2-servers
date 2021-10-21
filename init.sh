831601153a43a2e0c34d00fc577ad1f30810e359
aarmastah <aarpears@hotmail.com>
2021-10-21 15:38:38 -0400

    sourcebans

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install lib32z1 libncurses5:i386 libbz2-1.0:i386 lib32gcc-s1 lib32stdc++6 libtinfo5:i386 libcurl3-gnutls:i386 libsdl2-dev -y
+sudo apt install lib32z1 libncurses5:i386 libbz2-1.0:i386 lib32gcc-s1 lib32stdc++6 libtinfo5:i386 libcurl3-gnutls:i386 libsdl2-dev apache2 php libapache2-mod-php php-mysql -y
 sudo ufw allow 22
 sudo ufw allow 27015
+sudo ufw allow https
+sudo ufw allow http
 sudo ufw enable
 wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 tar zxf steamcmd_linux.tar.gz
