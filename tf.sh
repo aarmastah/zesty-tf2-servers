0832199b6b03728b632a51380740da42f83fee0f
aarmastah <aarpears@hotmail.com>
2021-11-21 23:29:31 -0500

    disable enablefakeip. this is not sdr related

--- tf.sh
+++ tf.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tf2/srcds_run -console -game tf -timeout 0 -autoupdate -steam_dir /hlserver -steamcmd_script /hlserver/tf2_ds.txt -nohltv +maxplayers 32 +map pl_badwater +sv_pure 0 -enablefakeip
+tf2/srcds_run -console -game tf -timeout 0 -autoupdate -steam_dir /hlserver -steamcmd_script /hlserver/tf2_ds.txt -nohltv +maxplayers 32 +map pl_badwater +sv_pure 0
